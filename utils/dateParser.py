29849e9960b13b3afae78616f28dd56a9219d87b
pablonvibe <73782244+pablonvibe@users.noreply.github.com>
2021-02-27 23:07:33 +0100

    Create dateParser.py

---
+++ utils/dateParser.py
@@ -0,0 +1,26 @@
+#!/home/michaelfareshi/.virtualenvs/myvenv/bin/python3.8
+# -*- coding: utf-8 -*-
+"""
+@author: pablo
+
+"""
+
+def dateParser(res):
+
+    parsed = list(res.split("-"))
+
+    month = int(parsed[1])
+    day = int(parsed[2])
+
+    parsedMonth = [int(x) for x in str(month)]
+    parsedDay = [int(x) for x in str(day)]
+
+    if parsedMonth[0]==0 :
+        month=parsedMonth[1]
+    if parsedDay[0]==0 :
+        day=parsedDay[1]
+
+
+    resPardesed = [month, day]
+    return resPardesed
+
